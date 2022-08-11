7120eeef0479419890c9a37deb1e09661e26fee3
Andrey <phobos05@bk.ru>
2022-08-11 08:36:31 +0700

    11_08_22  layout

--- m3_components/app/src/main/java/com/example/m3_components/MainActivity.kt
+++ m3_components/app/src/main/java/com/example/m3_components/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.m3_components
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.m3_components.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding=ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
     }
 }

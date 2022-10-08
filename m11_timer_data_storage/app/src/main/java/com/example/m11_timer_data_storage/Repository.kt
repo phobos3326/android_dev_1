e5a7c942175db77adcdc67443bdb1b7b4ce2446d
Andrey <phobos05@bk.ru>
2022-10-08 08:32:51 +0700

    8_10_22

--- m11_timer_data_storage/app/src/main/java/com/example/m11_timer_data_storage/Repository.kt
+++ m11_timer_data_storage/app/src/main/java/com/example/m11_timer_data_storage/Repository.kt
@@ -3,20 +3,16 @@ package com.example.m11_timer_data_storage
 import android.content.Context
 import android.content.SharedPreferences
 
-/*const val APP_PREF = "APP_PREF"
-const val KEY_PREF = "KEY_PREF"*/
 
-
-
+const val APP_PREF = "APP_PREF"
+const val KEY_PREF = "KEY_PREF"
 
 class Repository {
-    val APP_PREF = "APP_PREF"
-    val KEY_PREF = "KEY_PREF"
 
 
     private lateinit var preferences: SharedPreferences
 
-     fun getDataFromSharedPreference(context: Context): String? {
+    private fun getDataFromSharedPreference(context: Context): String? {
         val pref = context.getSharedPreferences(APP_PREF, Context.MODE_PRIVATE)
         return pref.getString(KEY_PREF, null)
 
@@ -27,18 +23,21 @@ class Repository {
     }
 
     fun saveText(text: String, context: Context) {
-
-      val prf=  context.getSharedPreferences(APP_PREF, Context.MODE_PRIVATE)
-
-            prf.edit()
-                .putString(KEY_PREF, text)
-                .apply()
+        val prf = context.getSharedPreferences(APP_PREF, Context.MODE_PRIVATE)
+        prf.edit()
+            .putString(KEY_PREF, text)
+            .apply()
     }
 
     fun clearText() {
     }
 
     fun getText(context: Context): String? {
-        return null
+
+        return when {
+            getDataFromSharedPreference(context) != null -> getDataFromSharedPreference(context)
+            getDataFromLocalVariable() != null -> getDataFromLocalVariable()
+            else -> "00000000000"
+        }
     }
 }

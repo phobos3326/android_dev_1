6b14b7d81a71741425d6f04dddd9909b9113f51f
Andrey <phobos05@bk.ru>
2023-02-16 23:26:17 +0700

    16_02_23 1

--- m17_recyclerview/app/src/main/java/com/example/m17_recyclerview/MainActivity.kt
+++ m17_recyclerview/app/src/main/java/com/example/m17_recyclerview/MainActivity.kt
@@ -2,17 +2,17 @@ package com.example.m17_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.m17_recyclerview.databinding.ActivityMainBinding
 import com.example.m17_recyclerview.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
--- m17_recyclerview/app/src/main/java/com/example/m17_recyclerview/ui/main/MainFragment.kt
+++ m17_recyclerview/app/src/main/java/com/example/m17_recyclerview/ui/main/MainFragment.kt
@@ -1,18 +1,22 @@
 package com.example.m17_recyclerview.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.fragment.*
-
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.m17_recyclerview.R
+
+
 import com.example.m17_recyclerview.databinding.FragmentMainBinding
 import com.example.m17_recyclerview.entity.ModelPhotos
 import kotlinx.coroutines.launch
+
 
 class MainFragment : Fragment() {
 
@@ -21,6 +25,7 @@ class MainFragment : Fragment() {
     private val viewModel: MainViewModel by viewModels()
 
     private val pageAdapter = MyAdapter { onItemClick(it) }
+
 
 
     companion object {
@@ -33,6 +38,10 @@ class MainFragment : Fragment() {
     ): View {
 
         _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+
+
+
         return binding.root
     }
 
@@ -48,7 +57,11 @@ class MainFragment : Fragment() {
 
     }
 
+
+
+
     private fun onItemClick(item: ModelPhotos.Photo) {
-        findNavController().navigate(R.id.action_mainFragment_to_itemFragment)
+
+        findNavController().navigate(R.id.ItemFragment)
     }
 }
